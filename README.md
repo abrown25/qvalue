b8754e69a5ca745901a342aa0ba4b6302b6794b3
abrown25 <aabrown24@hotmail.com>
2014-06-09 19:42:18 +0100

    updated help

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-qvalue
-======
+Usage: large_q_value [options]
+Options:
+    --help     : Print help and quit
+    --header   : Input has header line
+    --smoother : Smoothing spline applied to log pi0 values
+    --robust   : More robust values for small p values
+    --pi0      : Use value of pi0 given (useful for recreating qvalue package results)
+    --lambda   : Either a fixed number or a sequence given 0,0.9,0.05
+    --param    : Print out parameter list to given file
+    --out      : file to write results to (default stdout)
+    --input    : file to take results from (must be specified, if not explicitly, the last parameter after all options have been parsed is used)
+    --col      : column with p values (default 1)
--- parse_arg.d
+++ parse_arg.d
@@ -8,16 +8,18 @@ import std.stdio;
 import std.c.stdlib : exit;
 import std.exception : enforce;
 
-static immutable string helpString = "Usage: largeQvalue [options]
+static immutable string helpString = "Usage: large_q_value [options]
 Options:
---help : Print help and quit
---header : Input has header line
---pi0 : Use value of pi0 given
---lambda : Either a fixed number or a sequence given 0,0.9,0.05
---param : Print out parameter list to given file
---out : file to write results to (default stdout)
---input : file to take results from (default stdin)
---col : column with p values (default 1)
+    --help     : Print help and quit
+    --header   : Input has header line
+    --smoother : Smoothing spline applied to log pi0 values
+    --robust   : More robust values for small p values
+    --pi0      : Use value of pi0 given (useful for recreating qvalue package results)
+    --lambda   : Either a fixed number or a sequence given 0,0.9,0.05
+    --param    : Print out parameter list to given file
+    --out      : file to write results to (default stdout)
+    --input    : file to take results from (must be specified, if not explicitly, the last parameter after all options have been parsed is used)
+    --col      : column with p values (default 1)
 ";
 
 class InputException : Exception {
